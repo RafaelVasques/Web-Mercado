443e625883ce14e9d836e8f8b60302b9d0c1b5e3
Rafael Vasques Pereira <rafael.vasques.pereira@gmail.com>
2022-06-24 03:12:03 -0300

    Criação do menu principal

--- src/components/MainNav/index.js
+++ src/components/MainNav/index.js
@@ -4,13 +4,20 @@ import "./style.css";
 export default function MainNav (){
     return(
         <nav className="Main-Nav">
-            {/* <ul>
-                <li>Item menu1</li>
-                <li>Item menu2</li>
-                <li>Item menu3</li>
-                <li>Item menu4</li>
-                <li>Item menu5</li>
-            </ul> */}
+            <ul>
+                <li><a href="#">Item menu1</a></li>
+                <li className="Menu-Dropdown">
+                    <span className="Dropdown-Button">Item menu2</span>
+                    <ul className="Dropdown-Content">
+                        <li><a href="#">Sub Item 1</a></li>
+                        <li><a href="#">Sub Item 2</a></li>
+                        <li><a href="#">Sub Item 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Item menu3</a></li>
+                <li><a href="#">Item menu4</a></li>
+                <li><a href="#">Item menu5</a></li>
+            </ul>
         </nav>
     );
 }
