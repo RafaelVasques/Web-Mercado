4a4d09cafae0d4ede48870bc5f18231c2b7bf36b
Rafael Vasques Pereira <rafael.vasques.pereira@gmail.com>
2022-06-26 17:25:27 -0300

    aninhamento dos componentes para melhor organizar o projeto

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 // import './style.css';
 
-import SearchBarCartAera from "../SearchBarCartArea";
-import MainNav from "../MainNav";
-import DeliveryAddress from "../DeliveryAddress";
+import SearchBarCartAera from "../Comps-Header/SearchBarCartArea";
+import MainNav from "../Comps-Header/MainNav"
+import DeliveryAddress from "../Comps-Header/DeliveryAddress";
 
 export default function Header (){
     return(
